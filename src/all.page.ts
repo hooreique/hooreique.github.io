df90e84f95d3acfbdf0b0575d0be3ac2e0095d81
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2025-01-26 13:49:02 +0900

    fix index.html issue

--- src/all.page.ts
+++ src/all.page.ts
@@ -4,7 +4,7 @@ export default function* ({ search, paginate }: Lume.Data) {
   for (
     const page of paginate(
       search.pages("layout=scenario.vto", "date=desc"),
-      { size: 5, url: (n) => `/all/${n}` },
+      { size: 5, url: (n) => `/all/${n}/` },
     )
   ) {
     yield page;
