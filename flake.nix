72fa13d9588e3a9966143c3571b618d5cc4bea3f
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2025-06-01 14:42:39 +0900

    update nixpkgs lock

--- flake.nix
+++ flake.nix
@@ -1,19 +1,14 @@
 {
   description = "hooreique's personal gist site";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.deno
-          ];
-        };
-      });
+  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = inputs.nixpkgs.legacyPackages.${system};
+  in {
+    devShells.default = pkgs.mkShell {
+      buildInputs = [ pkgs.deno ];
+    };
+  });
 }
