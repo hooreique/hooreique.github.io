d874e0cb73bc74e92cbc60490e7accb3a4b280f1
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2025-11-23 23:28:25 +0900

    use yield* in all.page.ts

--- src/all.page.ts
+++ src/all.page.ts
@@ -1,12 +1,8 @@
 export const layout = "all.vto";
 
 export default function* ({ search, paginate }: Lume.Data) {
-  for (
-    const page of paginate(
-      search.pages("layout=scenario.vto", "date=desc"),
-      { size: 5, url: (n) => `/all/${n}/` },
-    )
-  ) {
-    yield page;
-  }
+  yield* paginate(
+    search.pages("layout=scenario.vto", "date=desc"),
+    { size: 5, url: (n) => `/all/${n}/` },
+  );
 }
